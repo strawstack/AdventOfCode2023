04088ec730c1aa7c4af8458814943865ae55a733
RichardHayes <rhshadowgreen@gmail.com>
2023-12-04 23:42:25 -0800

    update part 2

--- day/5/p2.js
+++ day/5/p2.js
@@ -1,5 +1,37 @@
 (() => {
 
+    // ranges: [{source, length}, ...]
+    // map: {dest, source, ranges: [{dest, source, length}, ...]}
+    // Convert each range using information in map
+    // Returns: ranges converted to destination space
+    function applyMap(ranges, map) {
+        const map_ranges = map.ranges.sort((a, b) => a.source - b.source);
+
+        const dest_ranges = [];
+        for (let range of ranges) {
+            // TODO
+            // special case: is range source < first map_range.source
+            // for each map_range convert a piece of range
+            // update range.source value and pay attention to range.source + range.length  
+        }
+
+        return dest_ranges;
+    }
+
+    function getLocations({seed_data, maps}) {
+        let locations = [];
+        for (let range of seed_data) {
+            let ranges = [range];
+            for (let map of maps) {
+                ranges = applyMap(ranges, map);
+            }
+            for (let range of ranges) {
+                locations.push(range);
+            }
+        }
+        return locations;
+    }
+
     function sol(input) {
 
         Array.prototype.first = function (func) {
@@ -29,7 +61,14 @@
                 })
             );
         
-        const seed_data = data[0];
+        const seeds = data[0];
+        const seed_data = [];
+        for (let i = 0; i < data[0].length; i += 2) {
+            seed_data.push({
+                source: seeds[i],
+                length: seeds[i + 1]
+            });
+        }
 
         const maps = data.slice(1, data.length).map(item_map => {
             const {source, dest} = item_map[0];
@@ -40,7 +79,9 @@
             }
         });
 
-        return {seed_data, maps};
+        const locations = getLocations({seed_data, maps});
+
+        return locations.reduce((a, c) => a.source < c.source ? a.source : c.source);
     }
 
     const test_input = `seeds: 79 14 55 13
