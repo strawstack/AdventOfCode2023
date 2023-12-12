050674b298fc4b203667c1e5191084ea704a5cdc
RichardHayes <rhshadowgreen@gmail.com>
2023-12-11 17:56:01 -0800

    day 10 and 11 part 1 and 2

--- day/10/p2.js
+++ day/10/p2.js
@@ -1,6 +1,20 @@
 (() => {
 
     function sol(input) {
+        
+        const pipe_lookup = {
+            "|": [{x: 0, y: 1}, {x: 0, y: -1}],
+            "-": [{x: -1, y: 0}, {x: 1, y: 0}],
+            "L": [{x: 0, y: -1}, {x: 1, y: 0}],
+            "J": [{x: -1, y: 0}, {x: 0, y: -1}],
+            "7": [{x: -1, y: 0}, {x: 0, y: 1}],
+            "F": [{x: 1, y: 0}, {x: 0, y: 1}],
+            "S": true
+        };
+
+        const eq = ({x: ax, y: ay}, {x: bx, y: by}) => {
+            return ax === bx && ay === by;
+        };
 
         const add = ({x: ax, y: ay}, {x: bx, y: by}) => {
             return {
@@ -15,73 +29,149 @@
             return y >= 0 && y < grid.length && x >= 0 && x < grid[0].length; 
         };
 
-        const dirs = [
-            {x: 0, y: -1},
-            {x: 1, y: 0},
-            {x: 0, y: 1},
-            {x: -1, y: 0}
-        ];
-
-        const hash = coord => {
-            return JSON.stringify(coord);
-        };
-
-        const dfs = ({x: cx, y: cy}, visited) => {
-        
-            visited[hash({x: cx, y: cy})] = true;
-            
-            for (let {x: dx, y: dy} of dirs) {
-                const {x: nx, y: ny} = add({x: cx, y: cy}, {x: dx, y: dy});
-                if (!inBounds(nx, ny)) return false;
-
-                if (grid[ny][nx] !== ".") continue;
-                if (hash({x: nx, y: ny}) in visited) continue;
-
-                if (!dfs({x: nx, y: ny}, visited)) {
-                    return false;
-                }
+        const otherEnd = ({x: px, y: py}, {x: ex, y: ey}) => {
+            if (!inBounds(px, py) || grid[py][px] === ".") {
+                return null;
             }
-            return true;
+            const dir = {
+                x: ex - px,
+                y: ey - py
+            };
+            const symb = grid[py][px];
+            const coords = pipe_lookup[symb];
+            const other = eq(dir, coords[0]) ? coords[1] : coords[0];
+            return add(other, {x: px, y: py});
         };
 
-        const total_visited = {};
-        for (let y = 0; y < grid.length; y++) {
-            for (let x = 0; x < grid[0].length; x++) {
-                if (grid[y][x] !== ".") continue;
-                if (hash({x, y}) in total_visited) continue;
-
-                const visited = {};
-                if (dfs({x, y}, visited)) {
-                    for (let k in visited) {
-                        total_visited[k] = true;
+        const findStart = () => {
+            for (let y = 0; y < grid.length; y++) {
+                for (let x = 0; x < grid[0].length; x++) {
+                    if (grid[y][x] === "S") {
+                        return {x, y};
                     }
                 }
             }
-        }
+        };
 
-        for (let k in total_visited) {
-            console.log(k)
-        }
+        const hash = coord => JSON.stringify(coord);
 
-        return Object.keys(total_visited).length;
+        const collect = {};
+
+        const advance = ({x: tx, y: ty}, {x: ex, y: ey}, steps) => {
+            collect[hash({x: ex, y: ey})] = true;
+            if (eq(findStart(), {x: tx, y: ty})) {
+                return steps;
+            } else {
+                const other = otherEnd({x: tx, y: ty}, {x: ex, y: ey});
+                return advance(other, {x: tx, y: ty}, steps + 1);
+            }
+        };
+
+        const count = () => {
+            let total = 0;
+            for (let y = 0; y < grid.length; y++) {
+                let inLoop = false;
+                let inter = null;
+                for (let x = 0; x < grid[0].length; x++) {
+                    const hh = hash({x, y});
+                    const ic = hh in collect;
+                    let symb = grid[y][x];
+                    
+                    if (symb === "S") {
+                        symb = "|"; // Hack: replace S with correct pipe
+                    }
+
+                    if (ic) {
+
+                        if (inLoop) {
+                            if (symb === "|") {
+                                inLoop = false;
+
+                            } else if (symb === "L") {
+                                inter = "L";
+
+                            } else if (symb === "F") {
+                                inter = "F";
+
+                            } else if (symb === "7") {
+                                inLoop = inter === "F";
+                                inter = null;
+
+                            } else if (symb === "J") {
+                                inLoop = inter === "L";
+                                inter = null;
+
+                            }
+
+                        } else { // !inLoop
+                            if (symb === "|") {
+                                inLoop = true;
+
+                            } else if (symb === "L") {
+                                inter = "L";
+
+                            } else if (symb === "F") {
+                                inter = "F";
+
+                            } else if (symb === "7") {
+                                inLoop = inter === "L";
+                                inter = null;
+
+                            } else if (symb === "J") {
+                                inLoop = inter === "F";
+                                inter = null;
+
+                            }
+                        }
+                    }
+
+                    if (inLoop && !ic) {
+                        total += 1;
+                        //console.log({x, y})
+                    }
+                }
+            }
+            return total;
+        };
+
+        const search = () => {
+            const dirs = [
+                {x: 0, y: -1},
+                {x: 1, y: 0},
+                {x: 0, y: 1},
+                {x: -1, y: 0}
+            ];
+            const {x: sx, y: sy} = findStart();
+            for (let {x: dx, y: dy} of dirs) {
+                const {x: tx, y: ty} = add({x: dx, y: dy}, {x: sx, y: sy});
+                
+                if (grid[ty][tx] in pipe_lookup) {
+                    advance({x: tx, y: ty}, {x: sx, y: sy}, 1);
+                    return count();
+                }
+            }
+            throw new Error("should not hit");
+        };
+
+        return search();
     }
 
-const test_input = `.F----7F7F7F7F-7....
-.|F--7||||||||FJ....
-.||.FJ||||||||L7....
-FJL7L7LJLJ||LJ.L-7..
-L--J.L7...LJS7F-7L7.
-....F-J..F7FJ|L7L7L7
-....L7.F7||L7|.L7L7|
-.....|FJLJ|FJ|F7|.LJ
-....FJL-7.||.||||...
-....L---J.LJ.LJLJ...
+const test_input = `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
 `;
 
+    // 394: not correct
     function main(input) {
         console.log(
-            //sol(input)
-            sol(test_input)
+            sol(input)
+            //sol(test_input)
         );
     }
 
---
+++ day/11/p2.js
@@ -0,0 +1,90 @@
+(() => {
+
+    function sol(input) {
+        let grid = input.trim().split("\n");
+
+        const hash = coord => JSON.stringify(coord);
+
+        const galaxies = {}; 
+        grid.forEach((row, y) => {
+            row.split("").forEach((symb, x) => {
+                if (symb === "#") {
+                    galaxies[hash({x, y})] = {x, y};
+                }
+            });
+        });
+
+        const gal_row = {};
+        const gal_col = {};
+        for (let {x, y} of Object.values(galaxies)) {
+            gal_row[y] = true;
+            gal_col[x] = true;
+        }
+
+        const free_row = {};
+        for (let r = 0; r < grid.length; r++) {
+            if (!(r in gal_row)) {
+                free_row[r] = true;
+            }
+        }
+        const free_col = {};
+        for (let c = 0; c < grid[0].length; c++) {
+            if (!(c in gal_col)) {
+                free_col[c] = true;
+            }
+        }
+
+        const eq = (a, b) => {
+            return a.x === b.x && a.y === b.y;
+        }
+
+        const EXPAND = 1000000 - 1;
+
+        const distance = (g1, g2) => {
+            const lx = (g1.x < g2.x) ? g1.x : g2.x; 
+            const ly = (g1.y < g2.y) ? g1.y : g2.y;
+            const hx = (g1.x < g2.x) ? g2.x : g1.x;
+            const hy = (g1.y < g2.y) ? g2.y : g1.y;
+            
+            let xd = hx - lx; 
+            let yd = hy - ly;
+
+            for (let y of Object.keys(free_row)) {
+                if (y > ly && y < hy) {
+                    yd += EXPAND;
+                }
+            }
+            for (let x of Object.keys(free_col)) {
+                if (x > lx && x < hx) {
+                    xd += EXPAND;
+                }
+            }
+            return xd + yd;
+        };
+
+        let total = 0;
+        const gal = Object.values(galaxies);
+        for (let i = 0; i < gal.length; i++) {
+            for (let j = i + 1; j < gal.length; j++) {
+                const g1 = gal[i];
+                const g2 = gal[j];
+                if (!eq(g1, g2)) {
+                    total += distance(g1, g2);
+                }
+            }
+        }
+
+        return total;
+    }
+
+    function main(input) {
+        console.log(
+            sol(input)
+        );
+    }
+
+    const DAY = 11;
+    const PART = 2;
+    solutions[DAY][PART] = main;
+    
+})();
